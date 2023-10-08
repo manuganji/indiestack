868ad105dbd2685b0295a94cca15778199105b78
Manu Ganji <manuganji@gmail.com>
2023-10-08 21:51:05 +0300

    Removing force-dynamic

--- app/[d]/(auth)/sign-up/layout.tsx
+++ app/[d]/(auth)/sign-up/layout.tsx
@@ -4,8 +4,6 @@ import { getCurrentProperty } from "@/lib/domains";
 import { redirect } from "next/navigation";
 import { Fragment, ReactNode } from "react";
 
-export const dynamic = "force-dynamic";
-
 export default async function SignUpLayout({
 	params,
 	children,
